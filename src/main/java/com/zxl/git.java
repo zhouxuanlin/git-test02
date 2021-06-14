c97b3fa4264b8fc6076ddfa2c27a363848696adb
zxl <zxl.@163.com>
2021-06-14 14:15:09 +0800

    there git

--- src/main/java/com/zxl/git.java
+++ src/main/java/com/zxl/git.java
@@ -4,5 +4,6 @@ public class git {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("first hello");
+        System.out.println(" there hello");
     }
 }
